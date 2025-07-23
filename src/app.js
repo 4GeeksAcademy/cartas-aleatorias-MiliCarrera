aff6d5048640a5aa808f024fa30e9f07ecdefc3c
Mili83666 <milagrocaba@gmail.com>
2025-07-23 13:47:08 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  const suitsElements = document.querySelectorAll(".suit")
+  const number = document.getElementById("numero")
+
+  let cards = ["♦", "♥", "♠", "♣"];
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+
+    let singleCard = cards[Math.floor(Math.random() * cards.length)];
+    let singleNumber = numbers[Math.floor(Math.random() * numbers.length)];
+         
+  
+suitsElements.forEach(element => {
+  element.textContent = singleCard
+ 
+  if(singleCard == "♦" || singleCard == "♥"){
+     element.classList.add("text-danger")
+  }
+});
+
+number.innerHTML = singleNumber
+  
+
+        
+        };
+
+
